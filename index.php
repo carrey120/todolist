922207d2524c00a965ac1ddf06d264ede2a7ae8c
Carrey Cheang <carrey.120.cc@gmail.com>
2020-05-29 10:27:05 +0800

    todolist start

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php include('header.php') ?>
+
+
+<div id="panel">
+    <h1>Todo List</h1>
+    <div id="todo-list">
+        <ul>
+            <li>
+                <div class="checkbox"></div>
+                <div class="content">Lorem, ipsum dolor.</div>
+                <div class="actions">
+                    <div class="delete">x</div>
+                </div>
+            </li>
+            <li>
+                <div class="checkbox"></div>
+                <div class="content">Lorem, ipsum dolor.</div>
+                <div class="actions">
+                    <div class="delete">x</div>
+                </div>
+            </li>
+            <li>
+                <div class="checkbox"></div>
+                <div class="content">Lorem, ipsum dolor.</div>
+                <div class="actions">
+                    <div class="delete">x</div>
+                </div>
+            </li>
+            <li class='new'>
+                <div class="checkbox"></div>
+                <div class="content" contenteditable="true"></div>
+            </li>
+        </ul>
+    </div>
+
+</div>
+
+
+
+
+
+<?php include('footer.php') ?>
